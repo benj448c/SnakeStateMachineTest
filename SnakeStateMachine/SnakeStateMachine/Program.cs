cf11158aafc489244ea3a07a967c9ca5f164d4a2
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2020-09-14 11:08:25 +0200

    PUSH

---
+++ SnakeStateMachine/SnakeStateMachine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            worker worker1 = new worker();
+            worker1.Start();
+        }
+    }
+}
---
+++ SnakeStateMachine/SnakeStateMachine/worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeStateMachine
+{
+    public class worker
+    {
+        public void Start()
+        {
+            moving();
+        }
+
+        private void moving()
+        {
+            SnakeStateMachine snake = new SnakeStateMachine();
+            Console.WriteLine(snake.DirectionName);
+            while (true)
+            {
+                snake.changedirection(Console.ReadKey(true));
+                Console.WriteLine(snake.DirectionName);
+            }
+        }
+
+    }
+}
